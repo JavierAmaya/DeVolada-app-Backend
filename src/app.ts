7a20b1d210250a9210beaf97ecdfdf5361b9cb36
JavierAmaya <photojavhn@gmail.com>
2021-05-31 12:33:18 -0600

    more setups

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express,{Application} from "express";
+
+import bodyParser from "body-parser";
+import cors from "cors";
+
+import {config} from "dotenv";
+import {resolve} from "path";
+
+config({"path":resolve(__dirname,"../.env")});
+
+//import {createConnection, Connection} from "typeorm";
+
+import {MainController} from './controller/main.controller';
+
+class App{
+    public app: Application;
+    public main_controller: MainController;
+
+
+    constructor(){  
+        this.app = express();
+        this.setConfig();
+        //this.setDBConnection();
+        this.main_controller= new MainController(this.app);
+    }
+
+    private setConfig(){
+        this.app.use(bodyParser.json({limit:"50mb"}));
+        this.app.use(bodyParser.urlencoded({limit:"50mb"}));
+        this.app.use(cors());
+    }
+
+   /* private setDBConnection(){
+        createConnection().then(Connection =>{
+            console.log("BD connected");
+        });
+    }*/
+
+}
+
+export default new App().app;
